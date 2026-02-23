d35cb710d6533c0e6754dca5f6506d12faf53db1
Sarthak Chaudhary <chaudharysarthak65@gmail.com>
2026-02-23 16:14:41 +0530

    Use environment variable for API baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Assuming backend runs on 5000
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.request.use(
